66445a5b29b661c981c41016878f746ef110daa7
Carlos Roig <roigcarlo@gmail.com>
2021-12-03 12:08:27 +0100

    e.l.f.s > l.a.b.o

--- 3-2/code.py
+++ 3-2/code.py
@@ -1,11 +1,10 @@
 import sys
 import numpy as np
 
-def do(l,a,b,o):
-    for i in range(o.shape[0]):
-        if [o:=o[:,o[i,:]==l(o,a,b)[i]]] and o.shape[1]==1:
-            return int(''.join([str(x) for x in o.T.reshape(o.shape[0],)]), 2)
+def do(e,l,f,s):
+    for i in range(s.shape[0]):
+        if [s:=s[:,s[i,:]==e(s,l,f)[i]]] and s.shape[1]==1: return int(''.join([str(x) for x in s.T.reshape(s.shape[0],)]), 2)
 
-o2,co2 = map(lambda a: do(*a),[(l:=lambda m,a,b: [(a,b)[np.count_nonzero(r) >= m.shape[1]/2] for r in m],0,1,m:=np.genfromtxt(sys.argv[1], delimiter=1, dtype='int').T), (l,1,0,m)])
+o,c = map(lambda l: do(*l),[(e:=lambda m,l,f: [(l,f)[np.count_nonzero(r) >= m.shape[1]/2] for r in m],0,1,m:=np.genfromtxt(sys.argv[1], delimiter=1, dtype='int').T), (e,1,0,m)])
 
-print(f"Kowalsky, life support status!: {o2*co2}")
+print(f"Kowalsky, life support status!: {o*c}")
