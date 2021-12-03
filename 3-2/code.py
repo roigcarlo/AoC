ae3d65dbc32b1cfc88f349822bd4d84647381538
Carlos Roig <roigcarlo@gmail.com>
2021-12-03 12:21:53 +0100

    404 and Kowalsky

--- 3-2/code.py
+++ 3-2/code.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def do(e,l,f,s):
     for i in range(s.shape[0]):
-        if [s:=s[:,s[i,:]==e(s,l,f)[i]]] and s.shape[1]==1: return int(''.join([str(x) for x in s.T.reshape(s.shape[0],)]), 2)
+        if [s:=s[:,s[i,:]==e(s,l,f)[i]]] and s.shape[1]==1:return int(''.join([str(x) for x in s.T.reshape(s.shape[0],)]),2)
 
-o,c = map(lambda l: do(*l),[(e:=lambda m,l,f: [(l,f)[np.count_nonzero(r) >= m.shape[1]/2] for r in m],0,1,m:=np.genfromtxt(sys.argv[1], delimiter=1, dtype='int').T), (e,1,0,m)])
+o,c = map(lambda l:do(*l),[(e:=lambda m,l,f:[(l,f)[np.count_nonzero(r)>=m.shape[1]/2] for r in m],0,1,m:=np.genfromtxt(sys.argv[1],delimiter=1,dtype='int').T),(e,1,0,m)])
 
-print(f"R:{o*c}")
+print(f"Kowalsky!: {o*c}")
