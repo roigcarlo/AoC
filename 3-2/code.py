d5a165ead1e635ce368b73aa31798c4f33dd3c5a
Carlos Roig <roigcarlo@gmail.com>
2021-12-03 14:38:49 +0100

    Loops are for n00bs

--- 3-2/code.py
+++ 3-2/code.py
@@ -1,10 +1,7 @@
 import sys
 import numpy as np
 
-def do(e,l,f,s):
-    for i in range(s.shape[0]):
-        if [s:=s[:,s[i,:]==e(s,l,f)[i]]] and s.shape[1]==1:return int(''.join([str(x) for x in s.T.reshape(s.shape[0],)]),2)
+def do(e,l,f,s): return [t[0]] if not[t:=e(s,l,f)] or not[s:=s[:,s[0,:]==t[0]]] or s.shape[0] <2 else [t[0]] + do(e,l,f,s[1:])
+o,c = map(lambda l:int(''.join([str(w)for w in do(*l)]),2),[(e:=lambda m,l,f:[(l,f)[np.count_nonzero(r)>=m.shape[1]/2] for r in m],0,1,m:=np.genfromtxt(sys.argv[1],delimiter=1,dtype='int').T),(e,1,0,m)])
 
-o,c = map(lambda l:do(*l),[(e:=lambda m,l,f:[(l,f)[np.count_nonzero(r)>=m.shape[1]/2] for r in m],0,1,m:=np.genfromtxt(sys.argv[1],delimiter=1,dtype='int').T),(e,1,0,m)])
-
-print(f"Kowalsky!: {o*c}")
+print(f"Kowalsky, oxygen report!: {o*c}")
