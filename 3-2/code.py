bff02ef63464762ed97be087f92580795374e79d
Carlos Roig <roigcarlo@gmail.com>
2021-12-03 15:23:12 +0100

    Maybe loops were better...

--- 3-2/code.py
+++ 3-2/code.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 
-def do(e,l,f,s): return [t[0]] if not[t:=e(s,l,f)] or not[s:=s[:,s[0,:]==t[0]]] or s.shape[0] <2 else [t[0]] + do(e,l,f,s[1:])
+def do(e,l,f,s):return [w[0] for w in s[0:]] if not [t:=e(s,l,f)] or not[s:=s[:,s[0,:]==t[0]]] or s.shape[1]==1 else [t[0]] + do(e,l,f,s[1:])
 o,c = map(lambda l:int(''.join([str(w)for w in do(*l)]),2),[(e:=lambda m,l,f:[(l,f)[np.count_nonzero(r)>=m.shape[1]/2] for r in m],0,1,m:=np.genfromtxt(sys.argv[1],delimiter=1,dtype='int').T),(e,1,0,m)])
 
-print(f"Kowalsky, oxygen report!: {o*c}")
+print(f"Kowalsky!: {o*c}")
