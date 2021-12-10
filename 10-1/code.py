d5cb8fcb01e3b66a38214a7e291e69f428b38cb0
Carlos <roigcarlo@gmail.com>
2021-12-10 16:01:47 +0100

    spaghettize

--- 10-1/code.py
+++ 10-1/code.py
@@ -1,5 +1,1 @@
-import sys
-import regex
-import itertools
-
-print("Kowalsky, let's do some flexing!:", sum(list(itertools.chain(*list(itertools.chain(*[[list(filter(lambda x: x != 0,[(0,{')':3,']':57,'}':1197,'>':25137}[s[-1]])[{'(':')','[':']','{':'}','<':'>'}[s[0]]!=s[-1]] for s in m.captures(1)])) for m in regex.finditer('((?:\(|\[|\{|<)(?:(?R)*)(?:\)|\]|\}|>))', l.strip())] for l in open(sys.argv[1])]))))))
+import sys;import regex;import itertools;print("Kowalsky, let's do some flexing!:", sum(list(itertools.chain(*list(itertools.chain(*[[list(filter(lambda x: x != 0,[(0,{')':3,']':57,'}':1197,'>':25137}[s[-1]])[{'(':')','[':']','{':'}','<':'>'}[s[0]]!=s[-1]] for s in m.captures(1)])) for m in regex.finditer('((?:\(|\[|\{|<)(?:(?R)*)(?:\)|\]|\}|>))', l.strip())] for l in open(sys.argv[1])]))))))
