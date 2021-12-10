b5f44a1cd3c4284e9436dfdbad0534a8bc554d74
Carlos <roigcarlo@gmail.com>
2021-12-10 15:53:27 +0100

    Remove some spaces in the dics

--- 10-1/code.py
+++ 10-1/code.py
@@ -2,4 +2,4 @@ import sys
 import regex
 import itertools
 
-print("Kowalsky, let's do some flexing!:", sum(list(itertools.chain(*list(itertools.chain(*[[list(filter(lambda x: x != 0,[(0,{')':3  ,']':57  ,'}':1197,'>':25137}[s[-1]])[{'(':')','[':']' ,'{':'}' ,'<':'>'}[s[0]]!=s[-1]] for s in m.captures(1)])) for m in regex.finditer('((?:\(|\[|\{|<)(?:(?R)*)(?:\)|\]|\}|>))', l.strip())] for l in open(sys.argv[1])]))))))
+print("Kowalsky, let's do some flexing!:", sum(list(itertools.chain(*list(itertools.chain(*[[list(filter(lambda x: x != 0,[(0,{')':3,']':57,'}':1197,'>':25137}[s[-1]])[{'(':')','[':']','{':'}','<':'>'}[s[0]]!=s[-1]] for s in m.captures(1)])) for m in regex.finditer('((?:\(|\[|\{|<)(?:(?R)*)(?:\)|\]|\}|>))', l.strip())] for l in open(sys.argv[1])]))))))
