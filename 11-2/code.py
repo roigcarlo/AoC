74832243c32d612f284cc33d319273703ed46abd
Carlos <roigcarlo@gmail.com>
2021-12-11 13:00:35 +0100

    Elavorate version later

---
+++ 11-2/code.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+
+light = np.genfromtxt(sys.argv[1],delimiter=1,dtype='int')
+much_light = np.zeros((light.shape[0] + 2, light.shape[1] + 2))
+much_light.fill(None)
+much_light[1:light.shape[0]+1,1:light.shape[1]+1] = light
+
+total = 0
+s = 0
+
+while True:
+    much_light += 1
+    while [flashy := np.where(much_light>9)] and len(flashy[0]):
+        for i,j in zip(flashy[0], flashy[1]):
+            total += 1
+            much_light[i-1,j-1] += 1
+            much_light[i-1,j+0] += 1
+            much_light[i-1,j+1] += 1
+            much_light[i+0,j-1] += 1
+            much_light[i+0,j+1] += 1
+            much_light[i+1,j-1] += 1
+            much_light[i+1,j+0] += 1
+            much_light[i+1,j+1] += 1
+            much_light[i,j] = -100
+    if len(np.where(much_light < 0)[0]) == 100:
+        print("Kowalsky, disco party!:", s+1)
+        exit()
+    much_light[much_light < 0] = 0
+    s+=1
