ef9ebba5a6b2132f1ee39acfbb2d16fe42c56d7d
Carlos Roig <roigcarlo@users.noreply.github.com>
2021-12-13 16:26:04 +0100

    Adding CI (#1)
    Adding CI

--- 13/code2.py
+++ 13/code2.py
@@ -53,45 +53,50 @@ the_message_written_in_the_darkness_of_space = np.full((max_x+3, max_y+3), ' ', 
 for bh in black_holes:
     the_message_written_in_the_darkness_of_space[bh[0]+1, bh[1]+1] = '█'
 
+print("Kowalsky, look at the stars and tell me what you see!:")
+for y in range(max_y+3):
+    print(''.join(np.asarray(the_message_written_in_the_darkness_of_space.T[y])))
+
 # Display
-with curses_screen() as stdscr:
+if display_disco := False:
+    with curses_screen() as stdscr:
 
-    for i in range(0, curses.COLORS):
-        curses.init_pair(i + 1, i, -1)
+        for i in range(0, curses.COLORS):
+            curses.init_pair(i + 1, i, -1)
 
-    color_matrix = np.zeros((6,6,6), dtype='int')
-    for i in range(6):
-        for j in range(6):
-            for k in range(6):
-                color_matrix[i,j,k] = 17+i*36+j*6+k
+        color_matrix = np.zeros((6,6,6), dtype='int')
+        for i in range(6):
+            for j in range(6):
+                for k in range(6):
+                    color_matrix[i,j,k] = 17+i*36+j*6+k
 
-    color_matrix = np.repeat(color_matrix,7,axis=2)
-    win = curses.newwin(max_x+2, max_y+2, 0, 0)
+        color_matrix = np.repeat(color_matrix,7,axis=2)
+        win = curses.newwin(max_x+2, max_y+2, 0, 0)
 
-    ss, cx, cy, cz = 0,0,0,0
+        ss, cx, cy, cz = 0,0,0,0
 
-    while True:
-        stdscr.addstr(0, 0, "Kowalsky, look at the stars and tell me what you see!:")
-        
-        for y in range(max_y+3):
-            message = ''.join(np.asarray(the_message_written_in_the_darkness_of_space.T[y]))
-            for m in range(len(message)):
-                stdscr.addstr(y+1, m, message[m], curses.color_pair(
-                    color_matrix[
-                        (cz)%10     if (cz)%10     <  6 else 10-cz,
-                        (cy+y-3)%10 if (cy+y-3)%10 <  6 else 10-((cy+y-3)%10),
-                        (cx+m  )%78 if (cx+m  )%78 < 40 else 78-((cx+m  )%78)
-                    ]
-                ))
+        while True:
+            stdscr.addstr(0, 0, "Kowalsky, look at the stars and tell me what you see!:")
+            
+            for y in range(max_y+3):
+                message = ''.join(np.asarray(the_message_written_in_the_darkness_of_space.T[y]))
+                for m in range(len(message)):
+                    stdscr.addstr(y+1, m, message[m], curses.color_pair(
+                        color_matrix[
+                            (cz)%10     if (cz)%10     <  6 else 10-cz,
+                            (cy+y-3)%10 if (cy+y-3)%10 <  6 else 10-((cy+y-3)%10),
+                            (cx+m  )%78 if (cx+m  )%78 < 40 else 78-((cx+m  )%78)
+                        ]
+                    ))
 
-        if stdscr.getch() == ord('q'):
-            break
+            if stdscr.getch() == ord('q'):
+                break
 
-        cx = (cx+1) % 78
-        cy = (cy+1) % 10
-        cz = (cz+1) % 10
+            cx = (cx+1) % 78
+            cy = (cy+1) % 10
+            cz = (cz+1) % 10
 
-        ss = (ss+1) % 100 
+            ss = (ss+1) % 100 
 
-        stdscr.refresh()
-        time.sleep(1/18)
+            stdscr.refresh()
+            time.sleep(1/18)
