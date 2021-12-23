b6373e9ccdb4ca8c98a4034e653bedf03738a981
Carlos <roigcarlo@gmail.com>
2021-12-24 00:17:10 +0100

    Halved time

--- 23/code2.py
+++ 23/code2.py
@@ -3,12 +3,10 @@ import time
 import functools
 
 cost   = {'A':1,'B':10,'C':100,'D':1000}
-target = {2:'A',3:'B',4:'C',5:'D'}
-dest   = {'A':2,'B':3,'C':4,'D':5}
+target = {1:'A',2:'B',3:'C',4:'D'}
+dest   = {'A':1,'B':2,'C':3,'D':4}
 valid  = [1,1,0,1,0,1,0,1,0,1,1]
-align  = {2:2,3:4,4:6,5:8}
-
-global_min = {'value': sys.maxsize, 'recursions': 0}
+align  = {1:2,2:4,3:6,4:8}
 
 def pretty_print(stat):
     print("#"*13)
@@ -34,73 +32,76 @@ def can_pop(room, rid):
         if room[i] != '.' and room[i] == target[rid]: return -1 if all(e == room[i] for e in room[i:]) else i
     return index
 
+globals = {'rlevel': 0}
+
 @functools.cache
 def find_true_void(stat):
-    if stat[1] == ('.','.','.','.','.','.','.','.','.','.','.') and stat[2] ==('A', 'A', 'A', 'A') and stat[3] == ('B', 'B', 'B', 'B') and stat[4] == ('C', 'C', 'C', 'C') and stat[5] == ('D', 'D', 'D', 'D'):
-        return stat[0]
+    if stat[0] == ('.',)*11 and stat[1] == ('A',)*4 and stat[2] == ('B',)*4 and stat[3] == ('C',)*4 and stat[4] == ('D',)*4:
+        return 0
 
     stat_queue = {}
 
     # Move the ampis from the hallway
     found_correct_pos = False
     for i in range(11):
-        if stat[1][i] != '.':
-            room_dst = dest[stat[1][i]]
+        if stat[0][i] != '.':
+            room_dst = dest[stat[0][i]]
             
             pos_a, pos_b = i, align[room_dst]
+
             if pos_a > pos_b: pos_a, pos_b = pos_b, pos_a
             else: pos_a, pos_b = pos_a +1, pos_b + 1
 
-            if stat[1][pos_a:pos_b] == tuple(['.' for _ in range(abs(i-align[room_dst]))]):
+            if stat[0][pos_a:pos_b] == tuple(['.' for _ in range(abs(i-align[room_dst]))]):
                 push_depth = can_push(stat[room_dst], room_dst)
                 if push_depth != -1:
-                    new_stat = [stat[0], [a for a in stat[1]], [a for a in stat[2]], [a for a in stat[3]], [a for a in stat[4]], [a for a in stat[5]]]
-                    new_stat[0] += cost[stat[1][i]] * (1+push_depth+abs(align[room_dst]-i))
-                    new_stat[room_dst][push_depth] = stat[1][i]
-                    new_stat[1][i] = '.'
+                    new_stat = [[a for a in stat[0]], [a for a in stat[1]], [a for a in stat[2]], [a for a in stat[3]], [a for a in stat[4]]]
+                    move_cost = cost[stat[0][i]] * (1+push_depth+abs(align[room_dst]-i))
+                    new_stat[room_dst][push_depth] = stat[0][i]
+                    new_stat[0][i] = '.'
 
-                    if new_stat[0] not in stat_queue.keys(): stat_queue[new_stat[0]] = []
-                    stat_queue[new_stat[0]].append((new_stat[0],)+tuple([tuple([a for a in b]) for b in new_stat[1:]]))
+                    if move_cost not in stat_queue.keys(): stat_queue[move_cost] = []
+                    stat_queue[move_cost].append(tuple([tuple([a for a in b]) for b in new_stat]))
                     found_correct_pos = True
                     break
 
     if not found_correct_pos:
     # Move ampis out of the rooms
-        for i in range(5,1,-1):
+        for i in range(1,5,1):
             pop_depth = can_pop(stat[i], i)
             if pop_depth >= 0:
                 # Try to move it to the left hallway
                 for h in range(align[i]-1,-1,-1):
                     if valid[h]:
-                        if stat[1][h] == '.':
-                            new_stat = [stat[0], [a for a in stat[1]], [a for a in stat[2]], [a for a in stat[3]], [a for a in stat[4]], [a for a in stat[5]]]
-                            new_stat[0] += cost[stat[i][pop_depth]] * (1+pop_depth+abs(align[i]-h))
-                            new_stat[1][h] = new_stat[i][pop_depth]
+                        if stat[0][h] == '.':
+                            new_stat = [[a for a in stat[0]], [a for a in stat[1]], [a for a in stat[2]], [a for a in stat[3]], [a for a in stat[4]]]
+                            move_cost = cost[stat[i][pop_depth]] * (1+pop_depth+abs(align[i]-h))
+                            new_stat[0][h] = new_stat[i][pop_depth]
                             new_stat[i][pop_depth] = '.'
 
-                            if new_stat[0] not in stat_queue.keys(): stat_queue[new_stat[0]] = []
-                            stat_queue[new_stat[0]].append((new_stat[0],)+tuple([tuple([a for a in b]) for b in new_stat[1:]]))
+                            if move_cost not in stat_queue.keys(): stat_queue[move_cost] = []
+                            stat_queue[move_cost].append(tuple([tuple([a for a in b]) for b in new_stat]))
                         else:
                             break
                 
                 # Try to move it to the right hallway
                 for h in range(align[i]+1,11,1):
                     if valid[h]:
-                        if stat[1][h] == '.':
-                            new_stat = [stat[0], [a for a in stat[1]], [a for a in stat[2]], [a for a in stat[3]], [a for a in stat[4]], [a for a in stat[5]]]
-                            new_stat[0] += cost[stat[i][pop_depth]] * (1+pop_depth+abs(align[i]-h))
-                            new_stat[1][h] = new_stat[i][pop_depth]
+                        if stat[0][h] == '.':
+                            new_stat = [[a for a in stat[0]], [a for a in stat[1]], [a for a in stat[2]], [a for a in stat[3]], [a for a in stat[4]]]
+                            move_cost = cost[stat[i][pop_depth]] * (1+pop_depth+abs(align[i]-h))
+                            new_stat[0][h] = new_stat[i][pop_depth]
                             new_stat[i][pop_depth] = '.'
 
-                            if new_stat[0] not in stat_queue.keys(): stat_queue[new_stat[0]] = []
-                            stat_queue[new_stat[0]].append((new_stat[0],)+tuple([tuple([a for a in b]) for b in new_stat[1:]]))
+                            if move_cost not in stat_queue.keys(): stat_queue[move_cost] = []
+                            stat_queue[move_cost].append(tuple([tuple([a for a in b]) for b in new_stat]))
                         else:
                             break
 
     new_low_energy = sys.maxsize
     for energy_state in sorted(stat_queue):
         for stat in stat_queue[energy_state]:
-            new_energy = find_true_void(stat)
+            new_energy = energy_state + find_true_void(stat)
             new_low_energy = min(new_energy, new_low_energy)
 
     return new_low_energy
@@ -117,7 +118,7 @@ with open(sys.argv[1]) as data:
 
     hallway = tuple(list('.' * 11))
 
-    stat = (0, hallway, room1, room2, room3, room4)
+    stat = (hallway, room1, room2, room3, room4)
 
     energy = find_true_void(stat)
 
