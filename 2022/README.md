325096e747b678b0ca5bb578e0d769cdd35999d3
Carlos Roig <roigcarlo@gmail.com>
2022-12-01 11:15:51 +0100

    What is love?, baby don't rust me

---
+++ 2022/README.md
@@ -0,0 +1,6 @@
+# :santa: [AoC-2022](https://adventofcode.com/2022) :santa:
+⚙️ My solutions for AoC-2022
+
+⚙️ Carefuly written alongside the warm of a cozy fireplace in winter.
+
+⚙️ Real programmers use only RUST (I'm gonna regret this.)
---
+++ 2022/d01/src/main.rs
@@ -0,0 +1,54 @@
+use std::{io, io::prelude::*};
+
+fn read_input() -> Vec<i32> {
+    let mut rustic_bread = vec![0];
+    let mut elf_num = 0;
+
+    for line in io::stdin().lock().lines() {
+        let data = line.unwrap();
+        match data.as_str() {
+            "" => {
+                elf_num += 1;
+                rustic_bread.push(0);
+            },
+            _  => rustic_bread[elf_num] += data.parse::<i32>().unwrap(),
+        }
+    }
+
+    rustic_bread
+}
+
+fn part1(rustic_bread: &[i32]) -> Option<i32> {
+    rustic_bread.iter().max().copied()
+}
+
+fn part2(rustic_bread: &mut Vec<i32>) -> i32 {
+    rustic_bread.sort_by(|a, b| b.cmp(a));
+
+    let mut top_calories = 0;
+
+    for i in 0..3 {
+        top_calories += rustic_bread[i];
+    }
+
+    top_calories 
+}
+
+fn main() -> io::Result<()> {
+    let mut rustic_bread = read_input();
+    let max_calories = part1(&rustic_bread);
+    let top_calories = part2(&mut rustic_bread);
+
+    print!("Part1 | ");
+
+    match max_calories {
+        Some(max_calories)  => print!("Max Calories: {}\n", max_calories),
+        None                => print!("Fit and slick, I eat no shit\n"),
+    }
+
+    print!("Part2 | ");
+
+    print!("Top Calories: {}\n", top_calories);
+
+    Ok(())
+}
