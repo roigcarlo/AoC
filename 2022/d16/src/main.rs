323dd3c00959647f5756aa9f0facfad24aa85d58
Carlos Roig <roigcarlo@gmail.com>
2022-12-19 13:56:12 +0100

    Reasonable times

--- 2022/d16/src/main.rs
+++ 2022/d16/src/main.rs
@@ -17,6 +17,7 @@ struct CaveSystem {
     c_cave: Cave,
     s_rift: SpatialRift,
     c_max: usize,
+    g_max: usize,
 }
 
 trait Problem {
@@ -27,6 +28,20 @@ trait Problem {
     fn solve(&mut self, left: &Vec<String>, at: &String, current: &usize, limit: &usize, pre: &usize) -> usize;
     fn part1(&mut self, input: &Vec<String>);
     fn part2(&mut self, input: &Vec<String>);
+}
+
+fn h_sort(cs: &CaveSystem, v: &mut Vec<String>) {
+    v.sort_by(|a,b| {
+        let da = 30 - cs.s_rift.get(&("AA".to_string(),a.to_string())).unwrap();
+        let va = cs.c_cave.get(a).unwrap().valve;
+        let heuristic_a = da * va;
+
+        let db = 30 - cs.s_rift.get(&("AA".to_string(),b.to_string())).unwrap();
+        let vb = cs.c_cave.get(b).unwrap().valve;
+        let heuristic_b = db * vb;
+
+        heuristic_a.cmp(&heuristic_b)
+    } );
 }
 
 impl Problem for CaveSystem {
@@ -72,8 +87,10 @@ impl Problem for CaveSystem {
         if left.is_empty() {
             let my_press = self.c_cave.get(at).unwrap().valve;
             let pre = pre + my_press * (limit-current);
+            self.c_max = usize::max(self.c_max, pre);
             pre
         } else if current >= limit {
+            self.c_max = usize::max(self.c_max, *pre);
             *pre
         } else {
             left.iter().map(|to| {
@@ -84,9 +101,16 @@ impl Problem for CaveSystem {
                 
                 new_left.remove(index);
                 let pre = pre + my_press * (limit-current);
-                self.c_max = usize::max(self.c_max, pre);
-                
-                self.solve(&new_left, to, &(current + dist_to_next), limit, &pre)
+                let next = self.c_cave.get(to).unwrap().valve * (limit-(current+dist_to_next-1));
+
+                // Predict an upper bound if we take this branch
+                let predict = pre + next + new_left.iter().map(|x| self.c_cave.get(x).unwrap().valve).sum::<usize>() * (limit-(current+dist_to_next-1));
+  
+                if predict <= self.c_max {
+                    0
+                } else {
+                    self.solve(&new_left, to, &(current + dist_to_next), limit, &pre)
+                }
             }).max().unwrap()
         }
     }
@@ -99,7 +123,9 @@ impl Problem for CaveSystem {
     
         let at = "AA";
     
-        let left = self.c_cave.iter().filter(|(_,v)| v.valve != 0).map(|(k,_)| k.to_string()).collect::<Vec<String>>();
+        let mut left = self.c_cave.iter().filter(|(_,v)| v.valve != 0).map(|(k,_)| k.to_string()).collect::<Vec<String>>();
+        
+        h_sort(self, &mut left);
     
         let pressure = left.iter().map(|to| {
             let mut new_left = left.clone();
@@ -126,12 +152,18 @@ impl Problem for CaveSystem {
     
         for i in 1..=left.len()/2 {
             let perms = left.iter().combinations(i);
-            println!("{:?}", perms);
+
+            // Debug
+            // println!("{:?}", perms);
             
             for left_perm in perms {
-                let elf: Vec<String> = left_perm.iter().map(|s|s.clone().into()).collect();
-                let ele: Vec<String> = left.iter().filter(|x| elf.iter().position(|y| y == *x).is_none()).map(|s|s.clone().into()).collect();
+                let mut elf: Vec<String> = left_perm.iter().map(|s|s.clone().into()).collect();
+                let mut ele: Vec<String> = left.iter().filter(|x| elf.iter().position(|y| y == *x).is_none()).map(|s|s.clone().into()).collect();
     
+                h_sort(self, &mut elf);
+                h_sort(self, &mut ele);
+
+                self.c_max = 0;
                 let elf_p = elf.iter().map(|to| {
                     let mut new_left = elf.clone();
                     let index = new_left.iter().position(|x| x == to).unwrap();
@@ -140,15 +172,18 @@ impl Problem for CaveSystem {
                     
                 }).max().unwrap();
     
+                let l_off  = self.c_max;
+                self.c_max = self.g_max;
                 let ele_p = ele.iter().map(|to| {
                     let mut new_left = ele.clone();
                     let index = new_left.iter().position(|x| x == to).unwrap();
                     new_left.remove(index);
-                    self.solve(&new_left, to, &(1+self.s_rift.get(&(at.to_string(),to.to_string())).unwrap()), &26, &0)
+                    self.solve(&new_left, to, &(1+self.s_rift.get(&(at.to_string(),to.to_string())).unwrap()), &26, &l_off)
                     
                 }).max().unwrap();
     
-                max_press = usize::max(max_press, elf_p + ele_p);
+                max_press = usize::max(max_press, ele_p);
+                self.g_max = max_press;
             }
         }
     
@@ -177,6 +212,7 @@ fn main() -> io::Result<()> {
         c_cave: Cave::new(),
         s_rift: SpatialRift::new(),
         c_max: 0,
+        g_max: 0,
     };
 
     let t_p1 = Instant::now();
@@ -185,8 +221,7 @@ fn main() -> io::Result<()> {
     let e_p1 = t_p1.elapsed();
 
     let t_p2 = Instant::now();
-    // Too Slow
-    // cave_sys.part2(&input);
+    cave_sys.part2(&input);
     let sort = cave_sys.c_max;
     let e_p2 = t_p2.elapsed();
 
