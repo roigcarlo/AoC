d46a6c2c41d12d9ef57b62579f9733bde9250d9f
roigcarlo <roigcarlo@gmail.com>
2023-12-08 21:25:01 +0100

    Sanity--

--- 2023/d08/main.cpp
+++ 2023/d08/main.cpp
@@ -9,6 +9,14 @@
 #include "elf_perf.h"
 #include "elf_report.h"
 
+auto make_node(const std::string & s) {
+    return std::make_tuple(
+        std::string_view(s.begin() +  0, s.begin() +  3),
+        std::string_view(s.begin() +  7, s.begin() + 10),
+        std::string_view(s.begin() + 12, s.begin() + 15)
+    );
+}
+
 std::size_t to_key(const std::string_view & sv) {
     return (sv[0] - 'A') * 26 * 26 + (sv[1] - 'A') * 26 + (sv[2] - 'A');
 }
@@ -17,13 +25,7 @@ std::size_t part1(const std::vector<std::string> & fv) {
     std::array<std::size_t, 17576> lmap{};
     std::array<std::size_t, 17576> rmap{};
     
-    auto map = fv | std::views::drop(1) | std::views::transform([](const std::string & s) { 
-        return std::make_tuple(
-            std::string_view(s.begin() +  0, s.begin() +  3),
-            std::string_view(s.begin() +  7, s.begin() + 10),
-            std::string_view(s.begin() + 12, s.begin() + 15)
-        ); 
-    });
+    auto map = fv | std::views::drop(1) | std::views::transform(make_node);
 
     std::ranges::for_each(map, [&lmap, &rmap](const auto & v) {
         lmap[to_key(std::get<0>(v))] = to_key(std::get<1>(v));
@@ -49,13 +51,7 @@ std::size_t part2(const std::vector<std::string> & fv) {
     std::array<std::size_t, 17576> rmap{};
     std::array<std::size_t, 17576> emap{};
 
-    auto map = fv | std::views::drop(1) | std::views::transform([](const std::string & s) { 
-        return std::make_tuple(
-            std::string_view(s.begin() +  0, s.begin() +  3),
-            std::string_view(s.begin() +  7, s.begin() + 10),
-            std::string_view(s.begin() + 12, s.begin() + 15)
-        ); 
-    });
+    auto map = fv | std::views::drop(1) | std::views::transform(make_node);
 
     std::ranges::for_each(map, [&lmap, &rmap, &emap](const auto & v) {
         lmap[to_key(std::get<0>(v))] = to_key(std::get<1>(v));
@@ -65,24 +61,16 @@ std::size_t part2(const std::vector<std::string> & fv) {
 
     auto route = std::string_view(fv[0]);
     auto route_size = route.size();
-    auto calc_cycle = [&](const auto & v) { 
-        auto cur = to_key(std::get<0>(v));
-
-        std::size_t steps = 0;
-        
-        while (!emap[cur]) {
-            cur = *(route.cbegin() + (steps++ % route_size)) == 'L' ? lmap[cur] : rmap[cur];
-        }
-
-        return steps;
+    auto calc_cycle = [&](auto cur) { 
+        return 2 + *(std::views::iota(0) | std::views::take_while([&](const auto & i) { 
+            return !emap[cur = *(route.cbegin() + (i % route_size)) == 'L' ? lmap[cur] : rmap[cur]];
+        }) | std::views::reverse).begin();
     };
 
     auto cycles = map | std::views::filter([&](const auto & v) { return std::get<0>(v)[2] == 'A'; })     
-                      | std::views::transform(calc_cycle);
+                      | std::views::transform([&](const auto & v){ return calc_cycle(to_key(std::get<0>(v))); });
 
-    return std::accumulate(cycles.begin(), cycles.end(), static_cast<std::size_t>(1), [](const auto & a, const auto & b) { 
-        return std::lcm(a, b); 
-    });
+    return std::accumulate(cycles.begin(), cycles.end(), static_cast<std::size_t>(1), std::lcm<std::size_t, std::size_t>);
 }
 
 int main (int argc, char** argv) {
