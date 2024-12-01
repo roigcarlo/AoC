5a059e3816757b70ae26f2cc1732f0b6f3d20442
Carlos A. Roig Pina <croig@cimne.upc.edu>
2024-12-01 15:52:16 +0100

    d01

---
+++ 2024/d01/src/main.zig
@@ -0,0 +1,90 @@
+const std = @import("std");
+const print = std.debug.print;
+
+// Prevent the last char from corrupting the num. Probably much cleaner ways to do this.
+fn cleanLine(line: []u8) ![]const u8 {
+    if (@import("builtin").os.tag == .windows) {
+        return std.mem.trimRight(u8, line, "\r");
+    } else {
+        return line;
+    }
+}
+
+test "part 1" {
+    // Read a file
+    var file = try std.fs.cwd().openFile("data", .{});
+    defer file.close();
+
+    var buffered = std.io.bufferedReader(file.reader());
+    const buffredr = buffered.reader();
+
+    var buff: [1024]u8 = undefined;
+
+    var list_a = std.ArrayList(i32).init(std.testing.allocator);
+    var list_b = std.ArrayList(i32).init(std.testing.allocator);
+
+    defer list_a.deinit();
+    defer list_b.deinit();
+
+    while (try buffredr.readUntilDelimiterOrEof(buff[0..], '\n')) |line| {
+        var it = std.mem.tokenizeSequence(u8, try cleanLine(line), " ");
+
+        try list_a.append(try std.fmt.parseInt(i32, it.next().?, 10));
+        try list_b.append(try std.fmt.parseInt(i32, it.next().?, 10));
+    }
+
+    // Actually solving the problem
+    std.mem.sort(i32, list_a.items, {}, std.sort.asc(i32));
+    std.mem.sort(i32, list_b.items, {}, std.sort.asc(i32));
+
+    // Iterating over both lists using the iterator pattern
+    var sum: u32 = 0;
+    for (list_a.items, list_b.items) |x, y| {
+        sum += @abs(x - y);
+    }
+
+    // Print Solution
+    print("Solution P1: {}\n", .{sum});
+
+    // Check
+    try std.testing.expectEqual(sum, @as(i32, 765748));
+}
+
+test "part 2" {
+    // Read a file
+    var file = try std.fs.cwd().openFile("data", .{});
+    defer file.close();
+
+    var buffered = std.io.bufferedReader(file.reader());
+    const buffredr = buffered.reader();
+
+    var buff: [1024]u8 = undefined;
+
+    var list_a = std.ArrayList(i32).init(std.testing.allocator);
+    var countr = std.AutoHashMap(i32, i32).init(std.testing.allocator);
+
+    defer list_a.deinit();
+    defer countr.deinit();
+
+    while (try buffredr.readUntilDelimiterOrEof(buff[0..], '\n')) |line| {
+        var it = std.mem.tokenizeSequence(u8, try cleanLine(line), " ");
+
+        const idx: i32 = try std.fmt.parseInt(i32, it.next().?, 10);
+        const val: i32 = try std.fmt.parseInt(i32, it.next().?, 10);
+
+        try list_a.append(idx);
+        try countr.put(val, @as(i32, 1) + (countr.get(val) orelse 0));
+    }
+
+    // Iterating over both lists using the iterator pattern
+    var sum: i32 = 0;
+    for (list_a.items) |idx| {
+        sum += idx * (countr.get(idx) orelse 0);
+    }
+
+    // Print Solution
+    print("Solution P2: {}\n", .{sum});
+
+    // Check
+    try std.testing.expectEqual(sum, @as(i32, 27732508));
+}
